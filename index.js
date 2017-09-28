9a5d98c1390b1726a5fd7b16ac0f5521f316f283
Geir Gåsodden <geir.gasodden@pythonia.no>
2017-09-28 08:46:47 +0200

    Updates Node. Switches to markdown-it

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const readFileSync = require('fs').readFileSync
-const marked = require('marked')
+const md = require('markdown-it')()
 const { send } = require('micro')
 const resolveRequest = require('./lib/resolve-request')
 const handleStats = require('./lib/handle-stats')
@@ -18,8 +18,7 @@ module.exports = async (request, response) => {
     } else {
       logger('info', ['index', 'frontpage'])
       const readme = readFileSync('./README.md', 'utf-8')
-      const html = marked(readme)
-      send(response, 200, html)
+      send(response, 200, md.render(readme))
     }
   } catch (error) {
     logger('error', ['index', error])
